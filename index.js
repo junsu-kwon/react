8e4ea3e82a2623436ad741fd33215e86a7d88c33
junsu-kwon <kjs940322@gmail.com>
2023-01-30 02:00:54 +0900

    test

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
-let promise = new Promise(function (resolve, reject) {
-  // 프라미스가 만들어지면 executor 함수는 자동으로 실행됩니다.
+async function f() {
+  await new Promise((resolve, reject) =>
+    setTimeout(() => reject('4. 에러!'), 3000),
+  ).then(
+    (resolve) => console.log(`얘는 안뜰거야 ${resolve}`),
+    (error) => console.log(`얘가 뜨겠지 ${error}`),
+  );
 
-  // 1초 뒤에 일이 성공적으로 끝났다는 신호가 전달되면서 result는 '완료'가 됩니다.
-  setTimeout(() => {
-    
-    console.log(promise);
-    reject('에러발생');
-  }, 10000);
-});
+  return '얘는 f함수 반환값이야';
+}
 
-console.log(promise);
+let promise = f();
+
+console.log('얘는 f함수 반환값 promise야 : ', promise);
+
+promise.then(
+  (resolve) => console.log(resolve),
+  (error) => console.log(error),
+);
+
+console.log('얘는 그냥 한번 해보자');

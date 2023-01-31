96385e22c0e351ba5d4d402fdf4a60ba84ec0e9c
junsu-kwon <kjs940322@gmail.com>
2023-01-31 21:12:33 +0900

    test

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
-async function f() {
-  await new Promise((resolve, reject) =>
-    setTimeout(() => reject('4. 에러!'), 3000),
-  ).then(
-    (resolve) => console.log(`얘는 안뜰거야 ${resolve}`),
-    (error) => console.log(`얘가 뜨겠지 ${error}`),
-  );
+(async () => {
+  let url =
+    'https://api.gith123ub.com/rasdasdepos/javascript-tutorial/ko.javascri123123pt.info/co123mmits';
+  let response = fetch(url);
 
-  return '얘는 f함수 반환값이야';
-}
+  console.log(response);
 
-let promise = f();
+  response.then((response) => console.log(response));
 
-console.log('얘는 f함수 반환값 promise야 : ', promise);
 
-promise.then(
-  (resolve) => console.log(resolve),
-  (error) => console.log(error),
-);
+  // let promise = new Promise((resolve, reject) => {
+  //   setTimeout(() => resolve("완료!"), 3000)
+  // });
 
-console.log('얘는 그냥 한번 해보자');
+  // console.log(promise);
+  // let result = await promise; // 프라미스가 이행될 때까지 기다림 -> 프로미스 내에서만 실행이 잠시 '중단’
+  
+  // console.log(promise);
+  // console.log(result);
+
+
+})();
+
+
+// let commits = await response.json(); // 응답 본문을 읽고 JSON 형태로 파싱함
+
+// alert(commits[0].author.login);
